6b98a7e4236bc5c50228efd98ccc0bce10b785a1
hx-devops <devops@hydrax.io>
2024-05-05 07:16:18 +0700

    init terraform

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = "4.10.0"
+  }
+
+  backend "gcs" {
+    bucket = "cloud-build-terraform-backend"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = "${var.project_id}"
+  region  = "${var.region}"
+}
+
+resource "google_storage_bucket" "bucket" {
+  name     = "test-bucket-random-001122-22"
+  location = "europe-west2"
+}
