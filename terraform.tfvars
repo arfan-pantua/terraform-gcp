95e42c62b987f40edf67878df0015af49ab1ee22
arfan-pantua <arfanpantua@gmail.com>
2024-06-20 13:40:43 +0700

    cloud build

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,1 +1,3 @@
 project_id = "msglowid-dev-332604"
+gitlab_repo_uri = "https://git.msglow.cloud/greatnest/reborn-apps.git"
+gitlab_uri = "https://git.msglow.cloud"
--- var.tf
+++ var.tf
@@ -5,3 +5,17 @@ variable "project_id" {
   description = "Google account id where resources will be created"
   type        = string
 }
+
+
+variable "gitlab_repo_uri" {
+  type = string
+}
+
+variable "gitlab_uri" {
+  type = string
+}
+
+variable "build_location" {
+  type = string
+  default = "asia-southeast1"
+}
