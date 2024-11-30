1598f545a73fd9c5a000744bdc5868bb621d6a78
arfan-pantua <97816778+arfan-pantua@users.noreply.github.com>
2024-11-30 11:19:14 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,5 @@ module "cloud-run" {
   env_app_name = "app-service-dev-service"
   env_jwt_aud = "app-service-dev"
   env_jwt_expiration = "3600"
-  env_mongodb_url = "mongodb+srv://dev-it:410rt7J2MoeDv938@mdb44-kci-sgp1-0002-sellershop-c18dd8cc.mongo.ondigitalocean.com/admin?retryWrites=false&replicaSet=mdb44-kci-sgp1-0002-sellershop&readPreference=primary&srvServiceName=mongodb&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-1"
   env_mongodb = "seller_app"
 }
